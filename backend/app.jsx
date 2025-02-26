e62b3c67b586418d46064a81525e8cc7d3a7115a
Dinesh107 <dineshjr107@gmail.com>
2025-02-26 14:05:01 +0530

    second commit

--- backend/app.jsx
+++ backend/app.jsx
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const connectDatabase = require("./config/connectDatabase.jsx");
 dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 
 const products = require("./routes/product.jsx");
 const orders = require("./routes/order.jsx");
 
+connectDatabase();
+
+// middleware
+app.use(express.json());
+
+// routes
 app.use("/api/v1/", products);
 app.use("/api/v1/", orders);
 
--- backend/controllers/orderController.jsx
+++ backend/controllers/orderController.jsx
@@ -1,4 +1,9 @@
+const orderModel = require('../models/orderModel.jsx');
+
 exports.createOrder = (request, response, next) => {
+  
+  console.log(request.body, 'DATA');
+  
   response.json({
     success: true,
     message: "Order works successfully!!!",
